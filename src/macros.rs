bcfc30860df84ec9b71bd17a079731551de10a30
Neil Mitchell <ndmitchell@gmail.com>
2023-05-18 10:32:24 +0100

    Add an example of producing a Null in a json! literal
    I tried to do this, and while you can piece together the way to do this from the interpolation comment, it's the one piece of direct JSON syntax that isn't immediately obvious.

--- src/macros.rs
+++ src/macros.rs
@@ -2,6 +2,7 @@
 ///
 /// ```
 /// # use serde_json::json;
+/// # use serde_json::Value;
 /// #
 /// let value = json!({
 ///     "code": 200,
@@ -10,8 +11,9 @@
 ///         "features": [
 ///             "serde",
 ///             "json"
-///         ]
-///     }
+///         ],
+///         "a_null": Value::Null,
+///     },
 /// });
 /// ```
 ///
